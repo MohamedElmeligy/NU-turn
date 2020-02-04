929fe0014e5a53082f8872a5097928b0e12399a4
Me5a2000 <mostafa18822@gmail.com>
2020-02-04 20:48:12 +0200

    final App?

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/rendering.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:custom_splash/custom_splash.dart';
 
 import 'package:provider/provider.dart';
-
 import './providers/phone_auth.dart';
 
 import './screens/auth_screen.dart';
+import './screens/map_screen.dart';
 
 void main() {
   debugPaintSizeEnabled = false;
@@ -25,10 +27,29 @@ void main() {
     ),
   );
 
-  runApp(MyApp());
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: CustomSplash(
+      imagePath: 'assets/Logo.png',
+      backGroundColor: Colors.white,
+      animationEffect: 'fade-in',
+      logoSize: 180,
+      home: MyApp(),
+      customFunction: () {},
+      duration: 3000,
+      type: CustomSplashType.StaticDuration,
+    ),
+  ));
 }
 
 class MyApp extends StatelessWidget {
+  Future<bool> isAuth() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    if (prefs.containsKey('phone')) return true;
+    return false;
+  }
+
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
@@ -39,7 +60,10 @@ class MyApp extends StatelessWidget {
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: LoginPage(),
+        home: FutureBuilder(
+            future: isAuth(),
+            builder: (ctx, auth) =>
+                auth.data == true ? MapScreen() : LoginPage()),
       ),
     );
   }
